25eb1f1a035a984b7dc7520260d06a64a524041d
syariefff <146154300+syariefff@users.noreply.github.com>
2023-10-29 20:02:44 +0700

    nomer3_star-3.cpp

--- praktikumApminggu7/nomer3_star-3.cpp
+++ praktikumApminggu7/nomer3_star-3.cpp
@@ -3,31 +3,31 @@ using namespace std;
 
 void star3(int angka) {
     for (int i = 0; i < angka; i++) {
-        for (int j = 0; j < angka - i; j++) {
+        for (int j = 0; j <= angka - i; j++) {
             cout << "*";
         }
         for (int j = 0; j < 2 * i; j++) {
             cout << " ";
         }
-        for (int j = 0; j < angka - i; j++) {
+        for (int j = 0; j <= angka - i; j++) {
             cout << "*";
         }
         cout << endl;
     }
-
-    for (int i = 0; i < angka; i++) {
-        for (int j = 0; j < i + 1; j++) {
+    for (int i = angka; i >= 0; i--) {
+        for (int j = 0; j <= angka - i; j++) {
             cout << "*";
         }
-        for (int j = 0; j < 2 * (angka - i - 1); j++) {
+        for (int j = 0; j < 2 * i; j++) {
             cout << " ";
         }
-        for (int j = 0; j < i + 1; j++) {
+        for (int j = 0; j <= angka - i; j++) {
             cout << "*";
         }
         cout << endl;
     }
 }
+
 
 int main() {
     int angka;
